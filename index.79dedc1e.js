132f9bb53d2edf8464b073a405e4abc7cc245fac
an-marryKyslenko <olih.a@icloud.com>
2024-09-15 16:50:10 +0200

    make build

---
+++ index.79dedc1e.js
@@ -0,0 +1,2 @@
+const e=document.documentElement,c=document.querySelector(".icon--menu"),n=document.querySelector(".icon--close");c.addEventListener("click",()=>{e.style.overflow="hidden"}),n.addEventListener("click",()=>{e.style.overflow="visible"});
+//# sourceMappingURL=index.79dedc1e.js.map
